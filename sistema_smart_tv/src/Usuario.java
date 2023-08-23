e6b4f73941b58567f4f134f22ca0a99927a333c4
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-08-23 14:46:06 -0300

    implementando smarttv

---
+++ sistema_smart_tv/src/Usuario.java
@@ -0,0 +1,9 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV LIGADA? " + smartTv.ligada);
+        System.out.println("CANAL ATUAL: " + smartTv.canal);
+        System.out.println("VOLUME ATUAL: " + smartTv.volume);
+    }   
+}
